3ff428dd618ccf2d34af38b945e4324a929b2fd7
Aleksander Aleksic <aleksander@aleksic.no>
2022-11-27 16:43:06 +0100

    Use state machine to start lambda layer build

--- modules/aft-lambda-layer/iam.tf
+++ modules/aft-lambda-layer/iam.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "codebuild_invoker_lambda_role" {
 }
 
 resource "aws_iam_role_policy" "codebuild" {
-  role = aws_iam_role.codebuild.name
+  role   = aws_iam_role.codebuild.name
   policy = templatefile("${path.module}/iam/role-policies/codebuild.tpl", {
     "data_aws_partition_current_partition"      = data.aws_partition.current.partition
     "aws_region"                                = var.aws_region
@@ -24,18 +24,97 @@ resource "aws_iam_role_policy" "codebuild" {
   })
 }
 
+data "aws_iam_policy_document" "statemachine_invoker" {
+  statement {
+    actions = [
+      "states:StartExecution",
+      "states:ListExecutions"
+    ]
+    resources = [
+      aws_sfn_state_machine.codebuild_statemachine.arn
+    ]
+  }
+  statement {
+    actions = [
+      "states:DescribeExecution"
+    ]
+    resources = [
+      "arn:aws:states:${var.aws_region}:${data.aws_caller_identity.session.account_id}:execution:${aws_sfn_state_machine.codebuild_statemachine.name}:*"
+    ]
+  }
+  statement {
+    actions = [
+      "logs:CreateLogGroup"
+    ]
+    resources = [
+      "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.session.account_id}:*"
+    ]
+  }
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.session.account_id}:log-group:/aws/lambda/${aws_lambda_function.codebuild_statemachine_invoker.function_name}:*"
+    ]
+  }
+}
+
 resource "aws_iam_role_policy" "codebuild_invoker_policy" {
-  role = aws_iam_role.codebuild_invoker_lambda_role.name
-  policy = templatefile("${path.module}/iam/role-policies/codebuild-invoker.tpl", {
-    "data_aws_partition_current_partition" = data.aws_partition.current.partition
-    "aws_region"                           = var.aws_region
-    "account_id"                           = local.account_id
-    "codebuild_project_name"               = aws_codebuild_project.codebuild.name
-    "codebuild_invoker_function_name"      = local.codebuild_invoker_function_name
-  })
+  role   = aws_iam_role.codebuild_invoker_lambda_role.name
+  policy = data.aws_iam_policy_document.statemachine_invoker.json
 }
 
 resource "aws_iam_role_policy_attachment" "codebuild_invoker_VPC_access" {
   role       = aws_iam_role.codebuild_invoker_lambda_role.name
   policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+data "aws_iam_policy_document" "codebuild_statemachine_trust" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = [
+        "states.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "codebuild_statemachine" {
+  name               = "aft-lambda-layer-codebuild-statemachine"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_statemachine_trust.json
+}
+
+data "aws_iam_policy_document" "codebuild_statemachine" {
+  statement {
+    actions = [
+      "codebuild:StartBuild",
+      "codebuild:StopBuild",
+      "codebuild:BatchGetBuilds",
+      "codebuild:BatchGetReports"
+    ]
+    resources = [
+      "arn:aws:codebuild:${var.aws_region}:${data.aws_caller_identity.session.account_id}:project/${aws_codebuild_project.codebuild.name}"
+    ]
+  }
+  statement {
+    actions = [
+      "events:PutTargets",
+      "events:PutRule",
+      "events:DescribeRule"
+    ]
+    resources = [
+      "arn:aws:events:${var.aws_region}:${data.aws_caller_identity.session.account_id}:rule/StepFunctionsGetEventForCodeBuildStartBuildRule"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "codebuild_statemachine_policy" {
+  role   = aws_iam_role.codebuild_statemachine.name
+  policy = data.aws_iam_policy_document.codebuild_statemachine.json
 }
--- modules/aft-lambda-layer/lambda.tf
+++ modules/aft-lambda-layer/lambda.tf
@@ -1,12 +1,12 @@
 # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-resource "aws_lambda_function" "codebuild_invoker" {
+resource "aws_lambda_function" "codebuild_statemachine_invoker" {
   filename         = var.builder_archive_path
-  function_name    = local.codebuild_invoker_function_name
-  description      = "AFT Lambda Layer - CodeBuild Invoker"
+  function_name    = local.statemachine_invoker_function_name
+  description      = "AFT Lambda Layer - CodeBuild Statemachine Invoker"
   role             = aws_iam_role.codebuild_invoker_lambda_role.arn
-  handler          = "codebuild_invoker.lambda_handler"
+  handler          = "statemachine_invoker.lambda_handler"
   source_code_hash = var.builder_archive_hash
   memory_size      = 1024
   runtime          = "python3.8"
@@ -21,16 +21,25 @@ resource "aws_lambda_function" "codebuild_invoker" {
   }
 }
 
-data "aws_lambda_invocation" "invoke_codebuild_job" {
-  function_name = aws_lambda_function.codebuild_invoker.function_name
+resource "aws_lambda_invocation" "invoke_codebuild_job" {
+  function_name = aws_lambda_function.codebuild_statemachine_invoker.function_name
+
+  triggers = {
+    aft_lambda_layer_hash = md5(data.local_file.aft_lambda_layer.content_base64)
+  }
+
+  depends_on = [
+    aws_iam_role_policy.codebuild_invoker_policy
+  ]
 
   input = <<JSON
 {
-  "codebuild_project_name": "${aws_codebuild_project.codebuild.name}"
+  "stateMachineArn": "${aws_sfn_state_machine.codebuild_statemachine.arn}",
+  "aftLambdaLayerHash": "${md5(data.local_file.aft_lambda_layer.content_base64)}"
 }
 JSON
 }
 
 output "lambda_layer_build_status" {
-  value = jsondecode(data.aws_lambda_invocation.invoke_codebuild_job.result)["Status"]
+  value = jsondecode(aws_lambda_invocation.invoke_codebuild_job.result)["Status"]
 }
